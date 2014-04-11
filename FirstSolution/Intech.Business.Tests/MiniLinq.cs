2b6a7d2ce10c6289326aa4be319a1e1538388cfb
Stephane Miginiac <stephonline13@gmail.com>
2014-04-11 09:17:27 +0200

    Add minilinq and tests

---
+++ FirstSolution/Intech.Business.Tests/MiniLinq.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intech.Business.Tests
+{
+    public static class MiniLinqExtension
+    {
+        public static IEnumerable<TResult> Select<T, TResult>(this IEnumerable<T> e, Func<T, TResult> map)
+        {
+            foreach (var x in e)
+                yield return map(x);
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> e, Func<T, bool> filter)
+        {
+            foreach (var x in e)
+                if (filter(x)) yield return x;
+        }
+
+        public static IEnumerable<T> Limit<T>(this IEnumerable<T> e, int count)
+        {
+            foreach (var x in e)
+                if (--count >= 0) yield return x;
+                else break;
+        }
+
+        public static IEnumerable<T> Skip<T>(this IEnumerable<T> e, int count)
+        {
+            foreach (var x in e)
+                if (--count < 0) yield return x;
+            // Same as:
+            //using( var enumerator = e.GetEnumerator() )
+            //{
+            //    while( enumerator.MoveNext() )
+            //    {
+            //        if( --count < 0 ) yield return enumerator.Current;
+            //    }
+            //}
+        }
+
+        public static int Count(this IEnumerable e)
+        {
+            var enumerator = e.GetEnumerator();
+            int count = 0;
+            while (enumerator.MoveNext()) count++;
+            return count;
+        }
+    }
+}
---
+++ FirstSolution/Intech.Business.Tests/StartingWithLinq.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intech.Business.Tests
+{
+    [TestFixture]
+    public class StartingWithLinq
+    {
+        static IEnumerable<int> Numbers (int start, int count)
+        {
+            for (int i = start; i < count + start; i++)
+            {
+                yield return i;
+            }
+        }
+
+        static IEnumerable<int> SawTeeth (int width)
+        {
+            while (true)
+            {
+                for (int i = 0; i < width; i++ )
+                    yield return i;
+            }
+        }
+
+        static IEnumerable<int> RandomNumbers(int seed)
+        {
+            var r = new Random(seed);
+            for (;;) yield return r.Next();
+        }
+
+        static IEnumerable<T> Limit<T> (IEnumerable<T> enumerable, int limit)
+        {
+            foreach(var element in enumerable)
+            {
+                if (--limit >= 0) yield return element;
+                else break;
+            }
+        }
+
+        static IEnumerable<T> Skip<T>(IEnumerable<T> enumerable, int count)
+        {
+            foreach (var element in enumerable)
+            {
+                if (--count < 0) yield return element;
+            }
+        }
+
+        static int Count<T>(IEnumerable<T> enumerable)
+        {
+            using (var enumerator = enumerable.GetEnumerator())
+            {
+                int count = 0;
+                while (enumerator.MoveNext())
+                    count++;
+                return count;
+            }
+        }
+
+        [Test]
+        public void SimpleRanges()
+        {
+            CollectionAssert.AreEqual(Numbers(0, 5), new[] { 0, 1, 2, 3, 4 });
+            int notTooMuch = 200;
+
+            foreach (var i in SawTeeth(12))
+            {
+                Console.WriteLine(i);
+                if (--notTooMuch == 0) break;
+            }
+
+            foreach (var i in Limit(SawTeeth(12), 25))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        [Test]
+        public void SimpleRanges2()
+        {
+            CollectionAssert.AreEqual( Numbers( 0, 5 ), new[] { 0, 1, 2, 3, 4 } );
+
+            int notTooMuch = 10;
+            foreach( var i in SawTeeth( 12 ) )
+            {
+                Console.WriteLine( i );
+                if( --notTooMuch == 0 ) break;
+            }
+
+            var noLimit = SawTeeth( 3 );
+            var limited = MiniLinqExtension.Limit( noLimit, 9 );
+            CollectionAssert.AreEqual( limited, new[] { 0, 1, 2, 0, 1, 2, 0, 1, 2 } );
+
+            var onlyThe5Last = MiniLinqExtension.Skip( limited, 4 );
+            CollectionAssert.AreEqual( onlyThe5Last, new[] { 1, 2, 0, 1, 2 } );
+
+            var source = RandomNumbers( 154 );
+            var the13ThatInterestMe = MiniLinqExtension.Limit( MiniLinqExtension.Skip( source, 20 ), 13 );
+
+            var simpler = source.Skip( 20 ).Limit( 13 );
+        }
+
+        [Test]
+        public void MiniLinqInAction()
+        {
+            var evenRandomNumbers = RandomNumbers( 125 ).Where( x => x % 2 == 0 );
+            var simpler = RandomNumbers( 125 ).Select( x => x * 2.0 );
+            var toString = RandomNumbers( 125 ).Select( x => x.ToString() );
+
+            var complexSerie = RandomNumbers(12).Select( x => new { R = x, I = x * 2 } );
+
+            var basedOnComplex = complexSerie
+                                    .Where( c => c.I > 0 )
+                                    .Limit( 50 );
+
+            var linq = from x in RandomNumbers( 125 )
+                       where x > 7
+                       select x;
+
+        }
+
+    }
+}
